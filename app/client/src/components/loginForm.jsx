9fd2da01f0c195efd4ee56756ca5c588afaf5618
anthonypitts2022 <anthony.pitts@columbia.edu>
2019-11-21 09:41:45 -0500

    google sign in went through, just taking the json returned by google and adding that to the current user's cookie

--- app/client/src/components/loginForm.jsx
+++ app/client/src/components/loginForm.jsx
@@ -8,6 +8,8 @@ import { ApolloProvider } from "react-apollo";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import GoogleLogin from "react-google-login";
+import axios from 'axios';
+
 
 const userClient = new ApolloClient({
   uri: "http://localhost:3302/user"
@@ -22,31 +24,32 @@ class LoginForm extends Component {
     this.state = {};
 
     //bindings
-    this.onSignIn = this.onSignIn.bind(this);
-
+    this.responseGoogle = this.responseGoogle.bind(this);
 
   }
 
-  componentDidMount() {
-    gapi.signin2.render('g-signin2', {
-      'scope': 'https://www.googleapis.com/auth/plus.login',
-      'width': 200,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': this.onSignIn
-    });
-  }
+  responseGoogle(response) {
+    signIn = signIn.bind(this);
+    signIn(response);
+    async function signIn(response) {
+      try{
+        console.log(response);
+        var variables={"input": { "googleEmail": response.profileObj.email } };
 
-  onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    this.setState({email: profile.getEmail()});
-  }
+        //call add like mutation
+        var loginUserResponse = await axios.post("http://localhost:3302/login", variables);
 
+        //newLike holds the data of the newly created like
+        var newUser = loginUserResponse.data;
+        console.log(newUser);
+        window.location = "/";
+
+      }
+      catch(err) {
+        console.log(err);
+      }
+    }
+  };
 
 
   render(){
@@ -57,29 +60,47 @@ class LoginForm extends Component {
          <meta name="google-signin-client_id" content="476731474183-4tm8h88lu6tnba05q3e81ommb9g1t1oc.apps.googleusercontent.com"></meta>
          <script src="https://apis.google.com/js/platform.js" async defer></script>
        </a>
-       <div className="container">
-          <div className="col-md-6 mx-auto text-center">
-             <div className="jumbotron">
-                <h1 className="wv-heading--title">
-                   Create, Share, Explore Memes
-                </h1>
-                <p className="wv-heading--subtitle">
-                   Sign in with Google today.
-                </p>
-             </div>
-          </div>
-          <div className="row">
-             <div className="col-md-4 mx-auto">
-                <div className="g-signin2" data-onsuccess="onSignIn"></div>
-                <div className="col-md-12 ">
-                   <div className="login-or">
-                      <hr className="hr-or" />
-                   </div>
-                </div>
-               <p className="small mt-3">By signing in, you are indicating that you have read and agree to the <a href="" className="ps-hero__content__link">Terms of Use</a> and <a href="#">Privacy Policy</a>.</p>
-          </div>
-       </div>
-    </div>
+       <form className="bg-light" method="post">
+           <div className="container">
+              <div className="col-md-6 mx-auto text-center">
+                 <div className="jumbotron">
+                    <h1 className="wv-heading--title">
+                       Create, Share, Explore Memes
+                    </h1>
+                    <p className="wv-heading--subtitle">
+                       Sign in with Google today.
+                    </p>
+                 </div>
+              </div>
+              <div className="row">
+                 <div className="col-md-4 mx-auto">
+                     <GoogleLogin
+                       clientId="476731474183-u57mkkmp59abldh9ko2vvrk65f2md5i4.apps.googleusercontent.com"
+                       render={renderProps => (
+                         <button
+                           className="btn btn-primary btn-block mt-0"
+                           onClick={renderProps.onClick}
+                           disabled={renderProps.disabled}
+                         >
+                           Google Login
+                         </button>
+                       )}
+                       buttonText="Login"
+                       onSuccess={this.responseGoogle}
+                       onFailure={this.responseGoogle}
+                       cookiePolicy={"single_host_origin"}
+                     />
+                    <div className="g-signin2" data-onsuccess="onSignIn"></div>
+                    <div className="col-md-12 ">
+                       <div className="login-or">
+                          <hr className="hr-or" />
+                       </div>
+                    </div>
+                   <p className="small mt-3">By signing in, you are indicating that you have read and agree to the <a href="" className="ps-hero__content__link">Terms of Use</a> and <a href="#">Privacy Policy</a>.</p>
+              </div>
+           </div>
+        </div>
+        </form>
     </div>
     );
   }
--- app/services/user/user-service.js
+++ app/services/user/user-service.js
@@ -2,12 +2,54 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 require('module-alias/register')
 const rootPath = require('app-root-path')
 const { logger } = require('@lib/logger.js')
-
+const { createApolloFetch } = require('apollo-fetch');
 const mongoose = require('./config/mongoose.js')
 const express = require('./config/express.js')
 
 const db = mongoose()
 const app = express()
 const port = process.env.PORT || 3002
+
+
+//============  login user mutation call   ====================//
+
+app.route('/login').post(function(req, res) {
+
+  //calls database mutation
+  var fetch = createApolloFetch({
+    uri: "http://localhost:3302/user"
+  });
+  //binds the res of upload to fetch to return the fetch data
+  fetch = fetch.bind(res)
+  fetch({
+    query:
+    `
+    query googleLogin($googleEmail: String!){
+      user: googleLogin(googleEmail: $googleEmail){
+      id
+      errors{
+        msg
+      }
+      }
+    }
+    `,
+    variables: {
+      input: {
+        isLike: req.body.input.googleEmail,
+      }
+    }
+  })
+  .then(result => {
+    //result.data holds the data returned from the mutation
+    return res.status(200).send(result.data);
+  })
+});
+
+
+
+
+
+
+
 
 app.listen(port, () => logger.info(`User service started on port ${port}`))
